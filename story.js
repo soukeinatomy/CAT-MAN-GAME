184d85abc957764651b0e492bea4abfa80c995b6
Suki <soukeinatomy@gmail.com>
2023-01-16 22:33:13 -0500

    Story

--- story.js
+++ story.js
@@ -1,9 +1,3 @@
-
-
+const cat = localStorage.getItem("cat")
 document.getElementById("cat-image").src="./assets/images/cat"+cat+".png"
 document.getElementById("cat-name").innerHTML = localStorage.getItem("name")
-
-function changeCatName(e) {
-localStorage.setItem("name", e.target.value);
-
-}
