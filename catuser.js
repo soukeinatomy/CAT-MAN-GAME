bf40b3622d69ceb3b66783671451ee0008b6ba27
Suki <soukeinatomy@gmail.com>
2023-01-17 01:33:03 -0500

    Choose a cat

--- catuser.js
+++ catuser.js
@@ -1,6 +1,9 @@
+// Local storage allows the JavaScript sites to store and access the data without any expiration date.
+
 let current = 1;
 localStorage.setItem("cat", "1");
 
+//Select function return index of element, reference element id
 function selectCat(index) {
 document.getElementById("cat-"+current).classList.toggle("active")
 document.getElementById("cat-"+index).classList.toggle("active")
